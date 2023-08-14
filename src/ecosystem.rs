5edf8952a54e5c692af1d1a8a50d5fff97d1f112
sitegui <sitegui@sitegui.com.br>
2023-08-15 00:48:46 +0200

    Improve flow algorithm

--- src/ecosystem.rs
+++ src/ecosystem.rs
@@ -2,7 +2,6 @@ mod insect;
 mod water_cycle;
 mod water_flow;
 
-use crate::cell::CellWater;
 use crate::config::Config;
 use crate::ecosystem::water_cycle::WaterCycleSystem;
 use crate::ecosystem::water_flow::WaterFlowSystem;
@@ -14,15 +13,31 @@ pub fn spawn_ecosystem(config: Arc<Config>, map: Arc<RwLock<Map>>) {
     tokio::spawn(WaterCycleSystem::new(&config, map.clone()).run());
     tokio::spawn(WaterFlowSystem::new(&config, map.clone()).run());
 
-    tokio::spawn(async move {
-        let mut map = map.write().unwrap();
-        map.cells_mut()
-            .indexed_iter_mut()
-            .for_each(|(coords, cell)| {
-                if (coords.0 + coords.1) % 3 == 0 {
-                    cell.set_water(CellWater::Shallow);
-                }
-            });
-        map.notify_update();
-    });
+    // tokio::spawn(async move {
+    //     let mut map = map.write().unwrap();
+    //     map.cells_mut()
+    //         .indexed_iter_mut()
+    //         .for_each(|(coords, cell)| {
+    //             if (coords.0 + coords.1) % 2 == 0 {
+    //                 cell.set_water(CellWater::Shallow);
+    //             }
+    //         });
+    //     map.notify_update();
+    // });
+
+    // tokio::spawn(async move {
+    //     loop {
+    //         {
+    //             let mut map = map.write().unwrap();
+    //             let size = map.size();
+    //             map.cells_mut()[(size / 4, size / 4)].set_water(CellWater::Shallow);
+    //             map.cells_mut()[(3 * size / 4, size / 4)].set_water(CellWater::Shallow);
+    //             map.cells_mut()[(size / 4, 3 * size / 4)].set_water(CellWater::Shallow);
+    //             map.cells_mut()[(3 * size / 4, 3 * size / 4)].set_water(CellWater::Shallow);
+    //             map.notify_update();
+    //         }
+    //
+    //         time::sleep(Duration::from_millis(100)).await;
+    //     }
+    // });
 }
--- src/ecosystem/water_flow.rs
+++ src/ecosystem/water_flow.rs
@@ -1,5 +1,6 @@
 use crate::config::Config;
 use crate::map::Map;
+use ndarray::Array2;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use tokio::time;
@@ -7,14 +8,18 @@ use tokio::time;
 #[derive(Debug)]
 pub struct WaterFlowSystem {
     map: Arc<RwLock<Map>>,
-    water_flows: Vec<WaterFlow>,
-    tick: Duration,
+    water_flows: Array2<WaterFlow>,
+    tick_sleep: Duration,
+    tick: usize,
 }
 
 #[derive(Debug)]
 struct WaterFlow {
-    from: (usize, usize),
-    to: (usize, usize),
+    /// The cell coordinates that want to receive water from this one, in priority order
+    targets: Box<[(usize, usize)]>,
+    /// The tick in which this cell last received a water flowing into it. This is used so that
+    /// each cell can only give water if it hasn't received water this tick
+    last_received_tick: std::cell::Cell<usize>,
 }
 
 impl WaterFlowSystem {
@@ -25,30 +30,43 @@ impl WaterFlowSystem {
         Self {
             map,
             water_flows,
-            tick: Duration::from_secs(config.water_flow_tick_seconds),
+            tick_sleep: Duration::from_secs(config.water_flow_tick_seconds),
+            tick: 0,
         }
     }
 
     pub async fn run(mut self) {
         loop {
             self.flow();
-            time::sleep(self.tick).await;
+            time::sleep(self.tick_sleep).await;
         }
     }
 
     fn flow(&mut self) {
+        self.tick += 1;
+
+        let this_tick = self.tick;
         let mut map = self.map.write().unwrap();
         let cells = map.cells_mut();
         let mut flowed = 0;
 
-        for water_flow in &self.water_flows {
-            let from_water = &cells[water_flow.from].water();
-            let to_water = &cells[water_flow.to].water();
+        for (source, flow) in self.water_flows.indexed_iter() {
+            if flow.last_received_tick.get() == this_tick {
+                // Cannot give water if already received at this time
+                continue;
+            }
 
-            if let Some((drier, wetter)) = from_water.drier().zip(to_water.wetter()) {
-                cells[water_flow.from].set_water(drier);
-                cells[water_flow.to].set_water(wetter);
-                flowed += 1;
+            if let Some(drier) = cells[source].water().drier() {
+                for &target in flow.targets.iter() {
+                    let target_cell = &mut cells[target];
+                    if let Some(wetter) = target_cell.water().wetter() {
+                        target_cell.set_water(wetter);
+                        cells[source].set_water(drier);
+                        flowed += 1;
+                        self.water_flows[target].last_received_tick.set(this_tick);
+                        break;
+                    }
+                }
             }
         }
 
@@ -59,45 +77,43 @@ impl WaterFlowSystem {
     }
 
     /// Determine to which neighbors each cell will flow
-    fn determine_water_flows(max_radius: usize, map: &Map) -> Vec<WaterFlow> {
-        let mut flows = Vec::new();
-
-        for radius in (1..=max_radius).rev() {
-            Self::append_water_flows(radius, map, &mut flows);
-        }
-
-        tracing::info!("Prepared {} water flows", flows.len());
-        flows
-    }
-
-    /// Determine to which neighbor at a given radius each cell will flow
-    fn append_water_flows(radius: usize, map: &Map, flows: &mut Vec<WaterFlow>) {
+    fn determine_water_flows(max_radius: usize, map: &Map) -> Array2<WaterFlow> {
         let cells = map.cells();
-        let mut flows = Vec::new();
+        let size = map.size();
 
-        for ((i, j), cell) in cells.indexed_iter() {
-            let mut lowest_neighbor_height = cell.height();
-            let mut lowest_neighbor_coordinates = (i, j);
+        Array2::from_shape_fn(map.cells().dim(), |(i, j)| {
+            let height = cells[(i, j)].height();
+            let mut targets = Vec::new();
 
-            let start_i = i.saturating_sub(radius);
-            let end_i = (i + radius + 1).min(map.size());
-            for neighbor_i in start_i..end_i {
-                let height = cells[(neighbor_i, j)].height();
-                if height < lowest_neighbor_height {
-                    lowest_neighbor_height = height;
-                    lowest_neighbor_coordinates = (neighbor_i, j);
+            let mut maybe_add_target = |target, radius| {
+                let target_height = cells[target].height();
+                if target_height < height {
+                    targets.push((radius, target_height, target));
+                }
+            };
+
+            let start_i = i.saturating_sub(max_radius);
+            let end_i = (i + max_radius + 1).min(size);
+            for target_i in start_i..end_i {
+                let delta_i = target_i.abs_diff(i);
+                let max_delta_j = max_radius - delta_i;
+                let start_j = j.saturating_sub(max_delta_j);
+                let end_j = (j + max_delta_j + 1).min(size);
+
+                for target_j in start_j..end_j {
+                    let delta_j = target_j.abs_diff(j);
+                    maybe_add_target((target_i, target_j), delta_i + delta_j);
                 }
             }
 
-            if lowest_neighbor_coordinates != (i, j) {
-                flows.push(WaterFlow {
-                    from: (i, j),
-                    to: lowest_neighbor_coordinates,
-                });
-            }
-        }
+            // Consider first the targets closer to the cell (lesser radius). In case of a tie,
+            // consider first the targets with the least height
+            targets.sort_by_key(|&(radius, height, _)| (radius, height));
 
-        tracing::info!("Prepared {} water flows", flows.len());
-        flows
+            WaterFlow {
+                targets: targets.into_iter().map(|(_, _, target)| target).collect(),
+                last_received_tick: std::cell::Cell::new(0),
+            }
+        })
     }
 }
